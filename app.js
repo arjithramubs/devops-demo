35326262f7b3cafb1091de058d75208aa2a730ba
root <root@ip-172-31-14-92.ap-south-1.compute.internal>
2026-01-04 08:05:27 +0000

    Change message to verify CI/CD deployment

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from CI/CD pipeline 🚀');
+  res.send('Deployed via Jenkins pipeline ✅');
 });
 
 app.get('/health', (req, res) => {
